db86f7dc04154bf6c10b0b28da2c13d36327c788
kauan <kauancavalcante8p@gmail.com>
2024-11-05 15:27:39 -0300

    duplicar funcao

--- Alfabeto.java
+++ Alfabeto.java
@@ -8,4 +8,10 @@ public class Alfabeto {
             System.out.println(letra);
         }
     }
+
+    public static void jhhh() {
+        for (char letra = 'A'; letra <= 'Z'; letra++) {
+            System.out.println(letra);
+        }
+    }
 }
