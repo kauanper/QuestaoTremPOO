3a4bac03e05d3d101b2541229822e55d1bb535b2
kauan <kauancavalcante8p@gmail.com>
2024-11-05 15:20:14 -0300

    alfabeto

---
+++ Alfabeto.java
@@ -0,0 +1,11 @@
+public class Alfabeto {
+    public static void main(String[] args) {
+        imprimirAlfabeto();
+    }
+
+    public static void imprimirAlfabeto() {
+        for (char letra = 'A'; letra <= 'Z'; letra++) {
+            System.out.println(letra);
+        }
+    }
+}
